9e449253482c2e43278eca9188e834055d99511b
jahid1604091 <jh409780@gmail.com>
2024-05-08 14:55:54 +0300

    video responsive and font fix

--- src/Components/2-Image/Image.jsx
+++ src/Components/2-Image/Image.jsx
@@ -1,23 +1,20 @@
-import './Image.css'
-import img22 from '../Images/image 22.png'
-import React from 'react';
-import Video from '../Images/BackgroundVideo.mp4'
+import "./Image.css";
+import React from "react";
+import Video from "../Images/BackgroundVideo.mp4";
 
 const image = () => {
-    const videoUrl = 'https://drive.google.com/uc?id=1kFzP-wTdUpS1BM6gaDahIP9PZqS5WXZH';
- 
-    return (
-        <div>
-            <div className='image-body'>
-                <video className='img' controls={false} autoPlay muted loop>
-                    <source src={Video} type="video/mp4" />
-                </video>
-            </div>
-            <div className='image-text'>
-                <p className='image-p'>Technology Ecosystem for Smart Factories</p>
-            </div>
-        </div>
-    );
+  return (
+    <section className="showcase">
+      <video className="video" controls={false} autoPlay muted loop>
+        <source src={Video} type="video/mp4" />
+      </video>
+      <div className="overlay"></div>
+      <div className="tag-line">
+        <h2>Technology Ecosystem</h2>
+        <h3>for Smart Factories</h3>
+      </div>
+    </section>
+  );
 };
 
 export default image;
