c7199dcaf6e25a57f73d052d0f1f35678aaaa38c
jahid1604091 <jh409780@gmail.com>
2024-11-26 12:11:17 +0600

    leader image update

--- src/Components/17-OurLeaders/Ourleader.jsx
+++ src/Components/17-OurLeaders/Ourleader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CardL from "../10-Leaders/CardLeader";
-import imageface1 from "../Images/Leaders/image 62.png";
+import jamil from "../Images/Leaders/Jamil-Bhai.png";
+import raihan from "../Images/Leaders/Rakib-Bhai.png";
+import intekhab from "../Images/Leaders/Toni-Bhai.png";
 import arif from "../Images/Leaders/arif sir.jpg";
-import mamun from "../Images/Leaders/mamun.png";
-import plabon from "../Images/Leaders/plabon.jpg";
-import anik from "../Images/Leaders/anik.jpeg";
-import naib from "../Images/Leaders/naib.jpeg";
+import fahmida from "../Images/Leaders/female.png";
+
 import "./ourleader.css";
 const Ourleader = () => {
   return (
@@ -13,103 +13,81 @@ const Ourleader = () => {
       <p className="heading-ourLeaders">Our Leaders</p>
       <div className="contents-ourL">
         <div className="first-contents-ourL">
-         
           <CardL
-            title1="Dr. Arif Iftakher Mahmood
-"
-            title2="Co-founder and Chairman
-"
-            image={arif}
-            email="arifmahmood@factorynext.com.bd"
-            linkedIn=""
-          />
-           <CardL
             title1="Insan Arafat Jamil"
-            title2="Founder & CEO"
-            image={imageface1}
+            title2="Chairman & CEO"
+            image={jamil}
             email="jamil@factorynext.com.bd"
             linkedIn="https://www.linkedin.com/in/insan-arafat-jamil/"
           />
           <CardL
-            title1="Abdullah Al Mamun"
-            title2="Co-founder and CTO
-"
-            image={mamun}
-            email="mamun@factorynext.com.bd"
-            linkedIn="https://www.linkedin.com/in/abdullah-al-mamun-47924aa6"
+            title1="Intekhab Alam"
+            title2="Managing Director"
+            image={intekhab}
+            // email="intekhab@factorynext.com.bd"
+            // linkedIn="https://www.linkedin.com/in/insan-arafat-jamil/"
+          />
+          <CardL
+            title1="Raihan Rakib"
+            title2="Director, Business Developement"
+            image={raihan}
+            // email="raihan@factorynext.com.bd"
+            // linkedIn="https://www.linkedin.com/in/abdullah-al-mamun-47924aa6"
           />
         </div>
         <div className="second-contents-ourL">
           <CardL
-            title1="Plabon Dutta"
-            title2="Technical Director"
-            image={plabon}
-            email="plabon@factorynext.com.bd"
-            linkedIn="https://www.linkedin.com/in/plabon-dutta-29231931/"
+            title1="Dr. Arif Iftakher Mahmood"
+            title2="Co-founder and Chairman"
+            image={arif}
+            email="arifmahmood@factorynext.com.bd"
+            linkedIn=""
           />
           <CardL
-            title1="Anik Islam Sajib"
-            title2="Technical Director"
-            image={anik}
-            email="anik@factorynext.com.bd"
-            linkedIn="https://www.linkedin.com/in/anik-islam-shojib-495ba5104"
-          />
-          <CardL
-            title1="Naib Hossain Khan"
-            title2="Technical Director"
-            image={naib}
-            email="naib@factorynext.com.bd"
-            linkedIn="https://www.linkedin.com/in/naibkhan/"
+            title1="Fahmida Chowdhury"
+            title2="Director, Software Technology"
+            image={fahmida}
+            // email="fahmida@factorynext.com.bd"
+            // linkedIn="https://www.linkedin.com/in/anik-islam-shojib-495ba5104"
           />
         </div>
       </div>
 
       <div className="contentsOurL2">
-        
         <CardL
-          title1="Dr. Arif Iftakher Mahmood
-"
-          title2="Co-founder and Chairman
-"
+          title1="Insan Arafat Jamil"
+          title2="Chairman & CEO"
+          image={jamil}
+          email="jamil@factorynext.com.bd"
+          linkedIn="https://www.linkedin.com/in/insan-arafat-jamil/"
+        />
+        <CardL
+          title1="Intekhab Alam"
+          title2="Managing Director"
+          image={intekhab}
+          // email="intekhab@factorynext.com.bd"
+          // linkedIn="https://www.linkedin.com/in/insan-arafat-jamil/"
+        />
+        <CardL
+          title1="Raihan Rakib"
+          title2="Director, Business Developement"
+          image={raihan}
+          // email="raihan@factorynext.com.bd"
+          // linkedIn="https://www.linkedin.com/in/abdullah-al-mamun-47924aa6"
+        />
+        <CardL
+          title1="Dr. Arif Iftakher Mahmood"
+          title2="Co-founder and Chairman"
           image={arif}
           email="arifmahmood@factorynext.com.bd"
           linkedIn=""
         />
         <CardL
-          title1="Insan Arafat Jamil"
-          title2="Founder & CEO"
-          image={imageface1}
-          email="jamil@factorynext.com.bd"
-          linkedIn="https://www.linkedin.com/in/insan-arafat-jamil/"
-        />
-        <CardL
-          title1="Abdullah Al Mamun"
-          title2="Co-founder and CTO
-"
-          image={mamun}
-          email="mamun@factorynext.com.bd"
-          linkedIn="https://www.linkedin.com/in/abdullah-al-mamun-47924aa6"
-        />
-        <CardL
-          title1="Plabon Dutta"
-          title2="Technical Director"
-          image={plabon}
-          email="plabon@factorynext.com.bd"
-          linkedIn="https://www.linkedin.com/in/plabon-dutta-29231931/"
-        />
-        <CardL
-          title1="Anik Islam Sajib"
-          title2="Technical Director"
-          image={anik}
-          email="anik@factorynext.com.bd"
-          linkedIn="https://www.linkedin.com/in/anik-islam-shojib-495ba5104"
-        />
-        <CardL
-          title1="Naib Hossain Khan"
-          title2="Technical Director"
-          image={naib}
-          email="naib@factorynext.com.bd"
-          linkedIn="https://www.linkedin.com/in/naibkhan/"
+          title1="Fahmida Chowdhury"
+          title2="Director, Software Technology"
+          image={fahmida}
+          // email="fahmida@factorynext.com.bd"
+          // linkedIn="https://www.linkedin.com/in/anik-islam-shojib-495ba5104"
         />
       </div>
     </div>
